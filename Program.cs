9f752af90a8556b8b7cc27ee6c0b333d1f01da80
Isadora Kondo <kondoiqcdev@gmail.com>
2021-07-01 21:12:05 -0300

    executando

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace sequnciaCentena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor=ConsoleColor.DarkMagenta;
+            Console.ForegroundColor=ConsoleColor.Black;
+            Console.WriteLine("+---------------------+");
+            Console.WriteLine("|--Sequencia Centena--|");
+            Console.WriteLine("+---------------------+");
+            Console.ResetColor();
+
+            for(int i = 1 ;  i < 101 ; i++){
+                
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
